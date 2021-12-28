cd9d411c2b2bed787580fc36061bf84585e805ea
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-28 22:57:37 +0530

    style: made text sizes responsive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
 <div class="min-h-screen flex flex-col items-center justify-center mx-8 space-y-8">
-	<h1 class="text-5xl text-gray-100">Orbits UI 🚀</h1>
-	<h5 class="text-gray-400 text-xl text-center">
+	<h1 class="text-5xl md:text-6xl lg:text-7xl text-gray-100">Orbits UI 🚀</h1>
+	<h5 class="text-gray-400 text-xl md:text-2xl lg:text-3xl text-center">
 		React Component Library with accessible components, customizable with Tailwind CSS
 	</h5>
 	<div
 		class="rounded-full bg-blue-700 backdrop-filter backdrop-blur-xl border-white p-4 shadow-md shadow-blue-700"
 	>
-		<p class="text-2xl text-gray-300">Coming Soon!</p>
+		<p class="text-2xl md:text-3xl lg:text-4xl text-gray-300">Coming Soon!</p>
 	</div>
 </div>
