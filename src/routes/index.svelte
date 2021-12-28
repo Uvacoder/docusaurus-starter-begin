fe8df777f489953b302d0cc085608a56ae0c8257
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-12-28 22:59:57 +0530

    style: added a set width for description on medium screens and abov

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 	class="min-h-screen flex flex-col items-center justify-center mx-8 space-y-8 md:space-y-12 lg:space-y-16"
 >
 	<h1 class="text-5xl md:text-6xl lg:text-7xl text-gray-100">Orbits UI 🚀</h1>
-	<h5 class="text-gray-400 text-xl md:text-2xl lg:text-3xl text-center">
+	<h5 class="text-gray-400 text-xl md:text-2xl lg:text-3xl text-center md:w-[48rem]">
 		React Component Library with accessible components, customizable with Tailwind CSS
 	</h5>
 	<div
